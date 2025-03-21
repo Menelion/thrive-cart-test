6295fcee21261aabf97de6fac44eeb9397da8280
André Polykanine <ap@oire.me>
2025-03-21 22:41:29 +0100

    Trying to fix Py script

--- apitest.py
+++ apitest.py
@@ -6,10 +6,10 @@ BASE_URL = "http://localhost"
 
 # Add products
 r1 = session.post(f"{BASE_URL}/basket/add/R01")
-print(r1.json())
+print(r1.text())
 r2 = session.post(f"{BASE_URL}/basket/add/G01")
-print(r2.json())
+print(r2.text())
 
 # Get total
 r3 = session.get(f"{BASE_URL}/basket/total")
-print(r3.json())
+print(r3.text())
