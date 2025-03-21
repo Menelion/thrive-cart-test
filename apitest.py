615deec5afc3cbcc690e0646348d7d38fc866f7c
André Polykanine <andre@accessmind.net>
2025-03-21 22:55:26 +0100

    Fix Python

--- apitest.py
+++ apitest.py
@@ -6,10 +6,10 @@ BASE_URL = "http://localhost"
 
 # Add products
 r1 = session.post(f"{BASE_URL}/basket/add/R01")
-print(r1.text)
+print(r1.json())
 r2 = session.post(f"{BASE_URL}/basket/add/G01")
-print(r2.text)
+print(r2.json())
 
 # Get total
 r3 = session.get(f"{BASE_URL}/basket/total")
-print(r3.text)
+print(r3.json())
